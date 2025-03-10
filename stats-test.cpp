7126434089587881febdd03b422d9bb323c4ea04
art-pogorelov <art.pogorelov@gmail.com>
2025-03-10 16:10:09 +0100

    complete the code

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,21 +1,28 @@
 #include "stats.h"
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+
+#include <array>
 #include <cmath>
 
 TEST(Statistics, ReportsAverageMinMax) {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
-    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
-    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
-    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
+  constexpr auto kValues = std::array{1.5, 8.9, 3.2, 4.5};
+  constexpr auto kExpectedAverage = 4.525;
+  constexpr auto kExpectedMax = 8.9;
+  constexpr auto kExpectedMin = 1.5;
+
+  const auto [min, max, average] = Statistics::ComputeStatistics(kValues);
+  EXPECT_DOUBLE_EQ(min, kExpectedMin);
+  EXPECT_DOUBLE_EQ(max, kExpectedMax);
+  EXPECT_DOUBLE_EQ(average, kExpectedAverage);
 }
 
 TEST(Statistics, AverageNaNForEmpty) {
-    auto computedStats = Statistics::ComputeStatistics({});
-    // All fields of computedStats (average, max, min) must be
-    // NAN (not-a-number), as defined in math.h
-    
-    // Specify the EXPECT statement here.
-    // Use http://www.cplusplus.com/reference/cmath/isnan/
+
+  // All fields of computedStats (average, max, min) must be
+  // NAN (not-a-number), as defined in math.h
+  const auto [min, max, average] = Statistics::ComputeStatistics({});
+  EXPECT_TRUE(std::isnan(min));
+  EXPECT_TRUE(std::isnan(max));
+  EXPECT_TRUE(std::isnan(average));
 }
