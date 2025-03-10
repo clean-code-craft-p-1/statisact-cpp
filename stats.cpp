515e8d7323eb24a13f29ae472a0d702a3efd95c8
art-pogorelov <art.pogorelov@gmail.com>
2025-03-10 16:10:09 +0100

    complete the code

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,21 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+#include <numeric>
+#include <ranges>
+#include <span>
+
+namespace Statistics {
+
+Stats Statistics::ComputeStatistics(const std::span<const double> numbers) {
+  if (numbers.empty()) {
+    return {};
+  }
+
+  return Stats{
+      .min = std::ranges::min(numbers),
+      .max = std::ranges::max(numbers),
+      .average = std::accumulate(std::begin(numbers), std::end(numbers), 0.0) /
+                 static_cast<double>(numbers.size())};
 }
+
+} // namespace Statistics
--- stats.h
+++ stats.h
@@ -1,7 +1,15 @@
-#include <vector>
+#pragma once
+
+#include <cmath>
+#include <span>
 
 namespace Statistics {
-    // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
-}
+
+struct Stats {
+  double min{NAN};
+  double max{NAN};
+  double average{NAN};
+};
+
+[[nodiscard]] Stats ComputeStatistics(std::span<const double> numbers);
+} // namespace Statistics
